f260ec51e2a0721c9d562e925186ab81358127d7
George Kallis <giorgos.kallis.gr@gmail.com>
2019-05-06 14:56:20 +0100

    Logo and ImageLinkForm components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*  <Logo />
+      <Logo />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ const Navigation = () => {
         <nav style={{display:'flex', justifyContent:'flex-end'}}>
             <p className='f3 link dim black underline pa3 pointer'> Sign out </p>
         </nav>
-    )
+    );
 }
 
 export default Navigation;
